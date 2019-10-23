03383fc8dd29a125d3a866c8facf2d407e5ebb95
Roger Peixoto Duarte <duarterogerpeixoto@gmail.com>
2019-10-23 09:02:29 -0300

    first commit to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
+import Logo from './components/Logos'
 import './App.css';
 
 function App() {
+  const[upperChange,setUpperChange] = useState('')
+  const[lowerChange,setLowerChange] = useState('')
+  const[middleChange,setMiddleChange]= useState('')
+  const[borderExpansion,setBorderExpansion] = useState('')
+  const[hoverEffect,setHoverEffect] = useState('')
+  const[buttonFade,setButtonFade] = useState('')
+  const[logoMove,setLogoMove] = useState('')
+  const[techLogos,showTechLogos]=useState(false)
+
+  function startMode(){
+    setBorderExpansion('startAction')
+    setButtonFade('buttonFade')
+    setMiddleChange('middleExpansion')
+    setUpperChange('upperShrink')
+    setLowerChange('lowerShrink')
+    setLogoMove('logoMove')
+    showTechLogos(true)
+  }
+
+  function mouseOnStart(){
+    setHoverEffect('hoverEffect')
+  }
+  function mouseOffStart(){
+    setHoverEffect('')
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className={'upper '+upperChange}></div>
+        <div className={'middle '+middleChange}>
+          <div className={'column-1 '+borderExpansion}>
+            <div className={'buttonContainer '+buttonFade}>
+              <button className={'startButton '+hoverEffect}
+               onMouseEnter={()=>mouseOnStart()} 
+               onMouseLeave={()=>mouseOffStart()} 
+               onClick={()=>{startMode()}}>START</button>
+            </div>
+            {/* {techLogos ? <Logo/> : null} */}
+          </div>
+          <div className={'column-2 '+logoMove}>
+            <div className={'logoContainer ' }>
+              <img className='logo' src='https://i.imgur.com/9ip0F5Y.png' alt='logo'/>
+              <p>Web development done right</p>
+            </div>
+          </div>
+          <div className={'column-3 '+borderExpansion}>
+            <div className={'buttonContainer '+buttonFade}>
+              <button className={'startButton '+hoverEffect}
+               onMouseEnter={()=>mouseOnStart()} 
+               onMouseLeave={()=>mouseOffStart()} 
+               onClick={()=>{startMode()}}>START</button>
+            </div>
+          </div>
+        </div>
+        <div className={'lower '+lowerChange}></div>
       </header>
     </div>
   );
---
+++ src/components/Logos.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Logo from '../assets/logo-react-blue.svg'
+
+const Logos = (props)=>{
+    return(
+
+       <img src={Logo} alt='logo'/>
+    )
+}
+
+export default Logos
