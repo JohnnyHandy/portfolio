1c5165de53355ed13e6547a9922321b47357dab4
Roger Peixoto Duarte <duarterogerpeixoto@gmail.com>
2019-11-17 20:04:05 -0300

    several changes on the info part

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ function App() {
   const[logoMove,setLogoMove] = useState('')
   const[techLogos,showTechLogos]=useState(false)
   const[moveNav,setMoveNav] = useState('')
+  const[info,setInfo] = useState('')
+  const[infoContainerClass,setInfoContainerClass] = useState('')
 
   function startMode(){
     setBorderExpansion('startAction')
@@ -31,6 +33,21 @@ function App() {
     setHoverEffect('')
   }
 
+  function handleInfoContainerChanges(infoType){
+    if(info === ''){
+      setMoveNav('moveNav2')
+      setInfo(infoType)
+      setInfoContainerClass('load-info')
+    }else if(info === infoType){
+      setMoveNav('moveNav')
+      setInfo('')
+      setInfoContainerClass('')
+    }else if(info !== '' && info !==infoType){
+      setInfo(infoType)
+      setInfoContainerClass('load-info')
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -48,13 +65,49 @@ function App() {
           <div className={'column-2 '+logoMove}>
             <div className={'logoContainer ' }>
               <img className='logo' src='https://i.imgur.com/9ip0F5Y.png' alt='logo'/>
-              <p>Web development done right</p>
+              <div>Web development done right</div>
             </div>
             <div className={'navContainer '+moveNav}>
-              <div className='navItem'>About</div>
-              <div className='navItem'>Projects</div>
-              <div className='navItem'>Contact</div>
+              <div 
+              className={info === 'about' ? 'navItem activeItem': 'navItem'}
+              onClick={()=>handleInfoContainerChanges('about')}>About</div>
+              <div 
+              className={info==='projects' ? 'navItem activeItem':'navItem'}
+              onClick={()=>handleInfoContainerChanges('projects')}>Projects</div>
+              <div 
+              className={info==='contact' ? 'navItem activeItem':'navItem'}
+              onClick={()=>handleInfoContainerChanges('contact')}>Contact</div>
             </div>
+            {info=== '' ? null : info==='about' ? 
+            <div className={'infoContainer '+infoContainerClass}>
+              <div>
+                <img src='https://i.imgur.com/3sKFLrS.png' alt='infoImg'/>
+              </div>
+              <div className='about-text'>
+              Johnny Handy is an idea behind a dream.<br/>
+              A dream of coding, designing and creating the best interfaces for unique users with different necessities.<br/>
+              A dream of taking the best of technology and web services and bring it to benefit of  daily processes, contributing to growth and development.<br/>
+              My name is Roger Peixoto, and I am behind Johnny Handy's idea. Here, I work with the development of websites and web services, with a focus on User Experience oriented design.<br/> 
+              Feel free to get in touch if you feel that we can work and collaborate together!
+              </div>
+            </div> : info==='projects' ?
+            <div className={'infoContainer '+infoContainerClass}>
+              <div className='project-item'>Detalhe</div>
+              <div className='project-item'>Detalhe</div>
+              <div className='project-item'>Detalhe</div>
+            </div> : info==='contact' ?
+            <div className={'infoContainer '+infoContainerClass} >
+              <div className='contact-email'>
+                  Email
+              </div>
+              <div className='phone-number'>
+                  Phone
+              </div>
+              <div className='social-media'>
+                  Social Media
+              </div>
+            </div>: 
+            null}
           </div>
           <div className={'column-3 '+borderExpansion}>
             <div className={'buttonContainer '+buttonFade}>
